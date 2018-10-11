9d8e110e55c6f0ab89957b13b40ae8186e080446
Paul Campbell <nimbleltd@gmail.com>
2018-10-11 07:17:43 -0500

    Implemented edit form fields with prefilled values

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root "movies#index"
   get "movies" => "movies#index" 
   get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "movie_edit"
 end
